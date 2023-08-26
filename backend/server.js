bdb2899ef08a2c8156b27139b5f607dd4e985baf
chaudhuree <chaudhuree00@gmail.com>
2023-08-26 15:55:26 +0600

    4️⃣ db connection done, model created for user,chat and message

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-// const helmet = require("helmet");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
+
 
 dotenv.config();
 const app = express();
@@ -10,8 +12,10 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(helmet({crossOriginResourcePolicy: false}))
+app.use(helmet({crossOriginResourcePolicy: false}))
 
+// db connection
+connectDB();
 app.get("/", (req, res) => {
     res.send("server is on");
 });
