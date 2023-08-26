480139c8a7ea650588c8803068d3584192a76267
chaudhuree <chaudhuree00@gmail.com>
2023-08-26 22:49:49 +0600

    8️⃣ FE: registration done cloudinary setup done fully functional for registration

--- fontend/src/components/authentication/Register.jsx
+++ fontend/src/components/authentication/Register.jsx
@@ -3,6 +3,8 @@ import { FormControl, FormLabel } from "@chakra-ui/react";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { VStack } from "@chakra-ui/react";
 import { useState } from "react";
+import {useToast} from "@chakra-ui/react";
+import axios from "axios";
 
 const Register = () => {
     const [show, setShow] = useState(false);
@@ -15,13 +17,114 @@ const Register = () => {
     const [password, setPassword] = useState();
     const [pic, setPic] = useState();
     const [picLoading, setPicLoading] = useState(false);
-
+    const toast = useToast();
     const submitHandler = async () => {
-        console.log("submit");
+        setPicLoading(true);
+        if (!name || !email || !password || !confirmpassword) {
+            toast({
+                      title: "Please Fill all the Feilds",
+                      status: "warning",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            setPicLoading(false);
+            return;
+        }
+        if (password !== confirmpassword) {
+            toast({
+                      title: "Passwords Do Not Match",
+                      status: "warning",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            return;
+        }
+        console.log(name, email, password, pic);
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            const { data } = await axios.post(
+                "http://localhost:5000/api/v1/register",
+                {
+                    name,
+                    email,
+                    password,
+                    pic,
+                },
+                config
+            );
+            console.log(data);
+            toast({
+                      title: "Registration Successful",
+                      status: "success",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setPicLoading(false);
+
+        } catch (error) {
+            toast({
+                      title: "Error Occured!",
+                      description: error.response.data.message,
+                      status: "error",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            setPicLoading(false);
+        }
     };
 
-    const postDetails = (pic) => {
-        console.log('pic upload')
+    const postDetails = (pics) => {
+        setPicLoading(true);
+        if (pics === undefined) {
+            toast({
+                      title: "Please Select an Image!",
+                      status: "warning",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            return;
+        }
+        console.log(pics);
+        if (pics.type === "image/jpeg" || pics.type === "image/png") {
+            const data = new FormData();
+            data.append("file", pics);
+            data.append("upload_preset", "envelope");
+            data.append("cloud_name", "djftsbsuu");
+            fetch("https://api.cloudinary.com/v1_1/djftsbsuu/image/upload", {
+                method: "post",
+                body: data,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setPic(data.url.toString());
+                    console.log(data.url.toString());
+                    setPicLoading(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setPicLoading(false);
+                });
+        } else {
+            toast({
+                      title: "Please Select an Image!",
+                      status: "warning",
+                      duration: 5000,
+                      isClosable: true,
+                      position: "bottom",
+                  });
+            setPicLoading(false);
+            return;
+        }
     };
 
     return (
