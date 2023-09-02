65a1a1447959a11318e57860fa314a84156cc1a4
chaudhuree <chaudhuree00@gmail.com>
2023-09-03 00:51:44 +0600

    1️⃣2️⃣ BE: create group

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -3,7 +3,7 @@ const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 
 //@description     Create or fetch One to One Chat
-//@route           POST /api/chat/
+//@route           POST /api/v1/createoraccessonetoonechat
 //@access          Protected
 const accessChat = asyncHandler(async (req, res) => {
     const {userId} = req.body;
@@ -56,7 +56,7 @@ const accessChat = asyncHandler(async (req, res) => {
 });
 
 //@description     Fetch all chats for a user
-//@route           GET /api/chat/
+//@route           GET /api/v1/fetchchats
 //@access          Protected
 const fetchChats = asyncHandler(async (req, res) => {
     try {
@@ -81,4 +81,43 @@ const fetchChats = asyncHandler(async (req, res) => {
 });
 
 
-module.exports = {accessChat, fetchChats};
+//@description     Create New Group Chat
+//@route           POST /api/v1/creategroupchat
+//@access          Protected
+const createGroupChat = asyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({message: "Please Fill all the feilds"});
+    }
+    // convert string to array. cz from the fontend we are sending array of users.and we cannot send array from the fontend to the backend directly. so we have to convert it to string and then convert it back to array.
+    let users = JSON.parse(req.body.users);
+    // group chat must have more than 2 users
+    if (users.length < 2) {
+        return res
+            .status(400)
+            .send("More than 2 users are required to form a group chat");
+    }
+
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+                                                chatName   : req.body.name,
+                                                users      : users,
+                                                isGroupChat: true,
+                                                groupAdmin : req.user,
+                                            });
+
+        const fullGroupChat = await Chat.findOne({_id: groupChat._id})
+                                        .populate("users", "-password")
+                                        .populate("groupAdmin", "-password");
+
+        res.status(200).json(fullGroupChat);
+    }
+    catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+
+module.exports = {accessChat, fetchChats, createGroupChat};
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const {
           accessChat,
           fetchChats,
+          createGroupChat,
 
       } = require("../controllers/chatControllers");
 const {protect} = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/createoraccessonetoonechat",protect, accessChat);
-router.get("/fetchchats",protect, fetchChats);
+router.post("/createoraccessonetoonechat", protect, accessChat);
+router.get("/fetchchats", protect, fetchChats);
+router.post("/creategroupchat", protect, createGroupChat);
 
 
 module.exports = router;
