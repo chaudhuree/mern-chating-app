5c0d0d4ce99e3762fe0d7a3b5840bbc1934fbe04
chaudhuree <chaudhuree00@gmail.com>
2023-09-02 23:57:35 +0600

    1️⃣0️⃣ BE: access chat or create chat for one to one user

---
+++ backend/controllers/chatControllers.js
@@ -0,0 +1,58 @@
+const asyncHandler = require("express-async-handler");
+const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
+
+//@description     Create or fetch One to One Chat
+//@route           POST /api/chat/
+//@access          Protected
+const accessChat = asyncHandler(async (req, res) => {
+    const {userId} = req.body;
+
+    if (!userId) {
+        console.log("UserId param not sent with request");
+        return res.sendStatus(400);
+    }
+
+    const isChat = await Chat.find({
+                                       isGroupChat: false,
+                                       users      : {$all: [req.user._id, userId]},
+                                   })
+                             .populate({
+                                           path  : "users",
+                                           select: "-password",
+                                       })
+                             .populate({
+                                           path    : "latestMessage",
+                                           populate: {
+                                               path  : "sender",
+                                               select: "name pic email",
+                                           },
+                                       });
+
+
+    if (isChat.length > 0) {
+        res.send(isChat[0]);
+    }
+    else {
+        let chatData = {
+            chatName   : "sender",
+            isGroupChat: false,
+            users      : [req.user._id, userId],
+        };
+
+        try {
+            const createdChat = await Chat.create(chatData);
+            const FullChat = await Chat.findOne({_id: createdChat._id}).populate(
+                "users",
+                "-password"
+            );
+            res.status(200).json(FullChat);
+        }
+        catch (error) {
+            res.status(400);
+            throw new Error(error.message);
+        }
+    }
+});
+
+module.exports = {accessChat};
