fbd3af51d8b4c212294cd79a4966d45bd529bb29
chaudhuree <chaudhuree00@gmail.com>
2023-09-03 21:48:27 +0600

    1️⃣6️⃣ FE: create context. wrap the app and add fontawesome cdn.

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {  Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import ChatPage from "./pages/ChatPage.jsx";
 function App() {
@@ -8,12 +8,12 @@ function App() {
   return (
     <>
         <div className="App">
-            <Router>
+
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/chat" element={<ChatPage />} />
                 </Routes>
-            </Router>
+
         </div>
     </>
   )
--- fontend/src/main.jsx
+++ fontend/src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import {ChakraProvider} from '@chakra-ui/react'
+import ChatProvider from "./context/ChatProvider.jsx";
+import {BrowserRouter} from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider>
+            <BrowserRouter>
+                <ChatProvider>
+                    <App/>
+                </ChatProvider>
+            </BrowserRouter>
+        </ChakraProvider>
+    </React.StrictMode>,
 )
